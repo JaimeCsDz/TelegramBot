6b8a5a23daebcd90df1ed43d20fbaaa20701c2f1
Jaime Castañeda <jaimecastaneda133@gmail.com>
2024-12-29 16:06:03 -0500

    Primeras guias

--- bot.js
+++ bot.js
@@ -13,22 +13,17 @@ const bot = new TelegramBot(TELEGRAM_TOKEN, { polling: true });
 
 // Cargar las guías desde los archivos JSON
 const guiasVentas = JSON.parse(fs.readFileSync('guia_ventas.json', 'utf8'));
-// const guiasFinanzas = JSON.parse(fs.readFileSync('guia_finanzas.json', 'utf8'));
-// const guiasSoporte = JSON.parse(fs.readFileSync('guia_soporte.json', 'utf8'));
+const guiasApertura = JSON.parse(fs.readFileSync('guiaApertura.json', 'utf8'));
 
 const categorias = {
+  'abrir jornada': {
+    descripcion: 'Opciones relacionadas con la apertura de jornada.',
+    guias: guiasApertura
+  },
   ventas: {
     descripcion: 'Opciones relacionadas con ventas.',
     guias: guiasVentas
   },
-  // finanzas: {
-  //   descripcion: 'Opciones relacionadas con finanzas.',
-  //   guias: guiasFinanzas
-  // },
-  // soporte: {
-  //   descripcion: 'Opciones relacionadas con soporte.',
-  //   guias: guiasSoporte
-  // }
 };
 
 // Estado temporal para manejar la categoría seleccionada por cada usuario
@@ -38,7 +33,7 @@ const userState = {};
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
 
-  const welcomeMessage = `¡Hola, soy Chatsito! Pulsa sobre el botón "Comenzar" para ver las opciones disponibles o ingresa palabras clave para una búsqueda específica. ¡Estoy aquí para ayudarte!`;
+  const welcomeMessage = `¡Hola, soy un asistente virtual!🤖 Pulsa sobre el botón "Comenzar" para ver las opciones disponibles 📝 o ingresa palabras clave para una búsqueda específica 🔍. ¡Estoy aquí para ayudarte!😊`;
 
   const options = {
     reply_markup: {
@@ -62,15 +57,18 @@ bot.on('callback_query', (callbackQuery) => {
   const callbackData = callbackQuery.data;
 
   if (callbackData === 'comenzar') {
-    // Mostrar las categorías principales
-    const opcionesCategorias = Object.keys(categorias)
-    .map((key, index) => `${index + 1}. ${key.charAt(0).toUpperCase() + key.slice(1)}`)
-    .join('\n');
+    const categoriasKeys = Object.keys(categorias);
+    const opcionesCategorias = categoriasKeys
+      .map((key, index) => `${index + 1}. ${key.charAt(0).toUpperCase() + key.slice(1)}`)
+      .join('\n');
 
     bot.sendMessage(
       chatId,
-      `¡Hola! Estas son las categorías principales disponibles:\n\n${opcionesCategorias}\n\nEscribe el nombre de la categoría para ver las opciones dentro de ella.`
+      `¡Hola! Estas son las categorías principales disponibles:📋\n\n${opcionesCategorias}\n\nEscribe el número o el nombre de la categoría para ver las opciones dentro de ella.🤓`
     );
+
+    // Guardar las claves para validar la selección por número
+    userState[chatId] = { categoriasKeys };
   }
 });
 
@@ -84,59 +82,101 @@ bot.on('message', (msg) => {
     return;
   }
 
-  // Si el usuario selecciona una categoría principal
-  if (categorias[userMessage]) {
-    userState[chatId] = userMessage; // Guardar la categoría seleccionada
-    const categoria = categorias[userMessage];
+  // Obtener estado del usuario
+  const estado = userState[chatId];
 
-    const opcionesSubmenu = Object.keys(categoria.guias)
-      .map((key, index) => `${index + 1}. ${key.charAt(0).toUpperCase() + key.slice(1)}`)
-      .join('\n');
-
-    bot.sendMessage(
-      chatId,
-      `Has seleccionado la categoría: ${userMessage.charAt(0).toUpperCase() + userMessage.slice(1)}.\n\n${categoria.descripcion}\n\nAquí están las opciones disponibles:\n\n${opcionesSubmenu}\n\nEscribe el número de la opción para obtener más detalles.`
-    );
-  } 
-  // Si el usuario selecciona una opción dentro de la categoría
-  else if (/^\d+$/.test(userMessage) && userState[chatId]) {
-    const categoriaSeleccionada = userState[chatId];
+  // Si el usuario está en un estado de selección de categoría
+  if (estado && estado.seleccion) {
+    const categoriaSeleccionada = estado.seleccion;
     const guias = categorias[categoriaSeleccionada].guias;
-    const opcionIndex = parseInt(userMessage) - 1;
-    const claveGuia = Object.keys(guias)[opcionIndex];
 
-    if (claveGuia) {
-      const respuesta = guias[claveGuia];
-      bot.sendMessage(
-        chatId,
-        `${respuesta.descripcion}\n\nConsulta el PDF: ${respuesta.pdf}`
-      );
+    let opcionSeleccionada = null;
+
+    // Validar si el usuario ingresó un número
+    if (/^\d+$/.test(userMessage)) {
+      const opcionIndex = parseInt(userMessage) - 1;
+      const clavesGuias = Object.keys(guias);
+      if (opcionIndex >= 0 && opcionIndex < clavesGuias.length) {
+        opcionSeleccionada = clavesGuias[opcionIndex];
+      }
+    }
+
+    // Validar si el usuario ingresó el nombre exacto de la opción
+    if (!opcionSeleccionada && guias[userMessage]) {
+      opcionSeleccionada = userMessage;
+    }
+
+    if (opcionSeleccionada) {
+      const guiaSeleccionada = guias[opcionSeleccionada];
+      let mensajeRespuesta = `${guiaSeleccionada.descripcion}`;
+      if (guiaSeleccionada.pdf) {
+        mensajeRespuesta += `\n\nConsulta el PDF: ${guiaSeleccionada.pdf}`;
+      }
+
+      bot.sendMessage(chatId, mensajeRespuesta,  { parse_mode: 'Markdown' });
 
       // Reiniciar el estado del usuario y mostrar el mensaje de bienvenida
       delete userState[chatId];
       mostrarMensajeBienvenida(chatId);
     } else {
-      bot.sendMessage(chatId, 'Opción no válida. Por favor, intenta nuevamente.');
+      bot.sendMessage(chatId, 'Opción no válida ⚠️. Por favor, ingresa el número o el nombre correcto de la opción 🙄.');
     }
-  } 
-  // Si el usuario envía algo que no es categoría ni opción, buscar por palabras clave
-  else {
-    const claveGuia = buscarEnTodasLasGuias(userMessage);
-    if (claveGuia) {
-      const respuesta = claveGuia;
+    return;
+  }
+
+  // Si el usuario selecciona una categoría principal
+  if (estado && estado.categoriasKeys) {
+    const categoriasKeys = estado.categoriasKeys;
+
+    let categoriaSeleccionada = null;
+
+    // Validar si el usuario ingresó un número
+    if (/^\d+$/.test(userMessage)) {
+      const opcionIndex = parseInt(userMessage) - 1;
+      if (opcionIndex >= 0 && opcionIndex < categoriasKeys.length) {
+        categoriaSeleccionada = categoriasKeys[opcionIndex];
+      }
+    }
+
+    // Validar si el usuario ingresó el nombre de la categoría
+    if (!categoriaSeleccionada && categorias[userMessage]) {
+      categoriaSeleccionada = userMessage;
+    }
+
+    if (categoriaSeleccionada) {
+      const categoria = categorias[categoriaSeleccionada];
+      userState[chatId] = { seleccion: categoriaSeleccionada }; // Actualizar estado
+
+      const opcionesSubmenu = Object.keys(categoria.guias)
+        .map((key, index) => `${index + 1}. ${key.charAt(0).toUpperCase() + key.slice(1)}`)
+        .join('\n');
+
       bot.sendMessage(
         chatId,
-        `${respuesta.descripcion}\n\nConsulta el PDF: ${respuesta.pdf}`
+        `Has seleccionado la categoría: ${categoriaSeleccionada.charAt(0).toUpperCase() + categoriaSeleccionada.slice(1)} 😀.\n\n${categoria.descripcion}\n\nAquí están las opciones disponibles: 🤔\n\n${opcionesSubmenu}\n\nEscribe el número o el nombre de la opción para obtener más detalles 🤓.`
       );
     } else {
-      bot.sendMessage(chatId, 'No encontré información relacionada. Intenta con otra pregunta o selección.');
+      bot.sendMessage(chatId, 'Categoría no válida ⚠️. Por favor, ingresa el número o el nombre correcto 🙄.');
     }
+    return;
+  }
+
+  // Si el usuario envía algo que no es categoría ni opción, buscar por palabras clave
+  const claveGuia = buscarEnTodasLasGuias(userMessage);
+  if (claveGuia) {
+    const respuesta = claveGuia;
+    bot.sendMessage(
+      chatId,
+      `${respuesta.descripcion}\n\nConsulta el PDF: ${respuesta.pdf}`
+    );
+  } else {
+    bot.sendMessage(chatId, 'No encontré información relacionada. Intenta con otra pregunta o selección.');
   }
 });
 
 // Función para mostrar el mensaje de bienvenida
 function mostrarMensajeBienvenida(chatId) {
-  const welcomeMessage = `¡Hola, soy Chatsito! Pulsa sobre el botón "Comenzar" para ver las opciones disponibles o ingresa palabras clave para una búsqueda específica. ¡Estoy aquí para ayudarte!`;
+  const welcomeMessage = `¡Hola, soy un asistente virtual!🤖 Pulsa sobre el botón "Comenzar" para ver las opciones disponibles 📝 o ingresa palabras clave para una búsqueda específica 🔍. ¡Estoy aquí para ayudarte!😊`;
 
   const options = {
     reply_markup: {
@@ -153,7 +193,6 @@ function mostrarMensajeBienvenida(chatId) {
 
   bot.sendMessage(chatId, welcomeMessage, options);
 }
-
 
 // Función para buscar en todas las guías
 function buscarEnTodasLasGuias(mensaje) {
